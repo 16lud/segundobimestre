ec24bdd88265bda947db129104b2fa1e4e8063c7
ludmila <ludmila.tawane@escolar.ifrn.edu.br>
2024-09-04 09:37:12 -0300

    salvar

--- questaodeatividade05.js
+++ questaodeatividade05.js
@@ -1,25 +1,25 @@
-function listar() {
-    let post_id = document.getElementById('post_id').value;
-    let url = `https://worldcupjson.net/matches?postId${post_id}`;
+let url_final =`https://worldcupjson.net/matches`
+fetch(url_final)
+.then(response => response.json())
+.then(finais => exibir_final(finais));
 
-    fetch(url)
-    .then((response)=> response.json())//response.json() converte  a resposta em um objeto json
-    .then((json)=>list_matches(json));
-    
-}
-function list_matches(json) {
-    let resultado =document.getElementById('resultado');
-    resultado.innerHTML='';
-if (json && json.legth > 0){// aqui verifica se existe dados na resposta
-let finalMatch = json[json.length - 1];
-let campeao = finalMatch.home_team.name;
-let viceCampeao = finalMatch.away_team.name;
-resultado.innerHTML=`campeao:${campeao}, viceCampeao:${viceCampeao}`;
-
-}else{
-
-    resultado.textContent =' nada encontrado';
-
-}
-}
-
+function mostrar_final(finais){
+    let resultado = document.getElementById('resultado');
+for (let final of finais){
+    if(final.stage_name == "final"){
+        let div= document.createElement('div');
+        let campeao;
+        if (final.winner == final.home_team.name){
+            campeao=`campeão:<br>${final.home_team.name}</b><br><br>
+            vice-campeao: <br>${final.away_team.name}`
+            
+        }else{
+           campeao=`campeão: <br>${final.away_team.name}</br><br><br>
+           vice-campeão: <br>${final.home_team.name}`
+        }
+        div.innerHTML=`${final.home_team_country} ${final.home_team.goals}(${final.home_team.penalties}) X
+        (${final.away_team.penalties}) ${final.away_team.goals} ${final.away_team_country}
+        <br> <br>${campeao}  `;
+        resultado.appendChild(div)
+    }
+}}
